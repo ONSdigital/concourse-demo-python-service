c7a2b7ee294f92d8f169a4165b461c1417149e75
liamtoozer <liam.toozer@gmail.com>
2018-04-19 14:32:59 +0100

    Add more tests

--- concourse_demo_python_service/__init__.py
+++ concourse_demo_python_service/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
 
 from concourse_demo_python_service import animal_repository, colour_repository
+from concourse_demo_python_service.colour_repository import ColourNotFound
 
 app = Flask(__name__)
 
 
 @app.route('/coloured_animal')
 def coloured_animal():
-    colour = colour_repository.fetch_by_letter(request.args.get('colour'))
-    animal = animal_repository.fetch_by_letter(request.args.get('animal'))
+    try:
+        colour = colour_repository.fetch_by_letter(request.args.get('colour'))
+        animal = animal_repository.fetch_by_letter(request.args.get('animal'))
+    except ColourNotFound:
+        return 'Colour not found', 404
 
     return f'{colour} {animal}'
--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -2,6 +2,8 @@ import unittest
 from unittest import mock
 
 from concourse_demo_python_service import app
+from concourse_demo_python_service.colour_repository import ColourNotFound
+
 
 class TestAcceptance(unittest.TestCase):
 
@@ -27,3 +29,13 @@ class TestAcceptance(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b"Orange Hamster")
+
+    @mock.patch('concourse_demo_python_service.colour_repository')
+    @mock.patch('concourse_demo_python_service.animal_repository')
+    def test_404_when_colour_not_found(self, animal_repository, colour_repository):
+        colour_repository.fetch_by_letter.side_effect = ColourNotFound()
+        animal_repository.fetch_by_letter.return_value = 'Dog'
+
+        response = self.app.get("/coloured_animal?colour=O&animal=H")
+
+        self.assertEqual(response.status_code, 404)
